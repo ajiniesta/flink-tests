427775e8b8d4323ee4413ee9cdbb4fffc415fc55
Antonio J. Iniesta <antonioj.iniesta@gmail.com>
2015-12-14 22:35:03 +0100

    Adding Taxi Ride Cleansing Exercise

---
+++ src/main/java/com/iniesta/ftests/stream/taxi/TaxiRideCleansing.java
@@ -0,0 +1,38 @@
+package com.iniesta.ftests.stream.taxi;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class TaxiRideCleansing {
+
+	@SuppressWarnings("serial")
+	public static void main(String[] args) throws Exception {
+		
+		ParameterTool parameter = ParameterTool.fromArgs(args);
+		String input = parameter.getRequired("input");
+		int maxDelay = parameter.getInt("maxdelay", 60);
+		int servingSpeed = parameter.getInt("speed", 600);
+		
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		
+		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+		
+		DataStreamSource<TaxiRide> rides = env.addSource(new TaxiRideSource(input, maxDelay, servingSpeed));
+		
+		DataStream<TaxiRide> filter = rides.filter(new FilterFunction<TaxiRide>() {			
+			@Override
+			public boolean filter(TaxiRide ride) throws Exception {				
+				return GeoUtils.isInNYC(ride.startLon, ride.startLat) || GeoUtils.isInNYC(ride.endLon, ride.endLat);
+			}
+		});
+		
+		filter.print();
+		
+		env.execute("Ride Cleansing Exercise");
+	}
+}
